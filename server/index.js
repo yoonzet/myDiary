abc2001ffd11ec5c328d4d005d8136da00988ed3
yoonji lee <lalala703@naver.com>
2022-09-29 22:35:02 +0900

    index.js 수정

--- server/index.js
+++ server/index.js
@@ -31,13 +31,13 @@ app.listen(port, () => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
-  //
   app.get("*", (req, res) => {
     res.sendFile(
       // path.resolve(__dirname, "../client", "build", "index.html", "index.html")
-      path.resolve(__dirname + "/client/build/index.html", "index.html")
+      path.join(__dirname + "/client/build/index.html")
     );
   });
 }
