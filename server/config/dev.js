0674373e351d2880e09ac5cd633a2f05e8b9b8e7
yoonji lee <lalala703@naver.com>
2022-09-29 14:53:36 +0900

    commit message

---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://yoon:dlapdlf1@cluster0.2ac5iv3.mongodb.net/?retryWrites=true&w=majority",
+};
--- server/index.js
+++ server/index.js
@@ -29,3 +29,11 @@ mongoose
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
