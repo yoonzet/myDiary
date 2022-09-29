5b99e99eaba4d6e014058b837e2030a199c7a2f9
yoonji lee <lalala703@naver.com>
2022-09-29 22:07:30 +0900

    index.html추가입력

--- server/index.js
+++ server/index.js
@@ -35,6 +35,8 @@ if (process.env.NODE_ENV === "production") {
 
   //
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "../client", "build", "index.html", "index.html")
+    );
   });
 }
