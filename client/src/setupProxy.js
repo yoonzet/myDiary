36e69a7b70627c97eb92980ad6fa2c2e15d92a0b
yoonji lee <lalala703@naver.com>
2022-09-29 17:08:42 +0900

    make it better

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:4000",
+      // target: "http://localhost:4000",
+      target: "https://app-my-diary.herokuapp.com/",
       changeOrigin: true,
     })
   );
