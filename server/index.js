d15829421bac3a02513ebe79e1ca5942f60f661c
yoonji lee <lalala703@naver.com>
2022-09-29 19:35:19 +0900

    .

--- server/index.js
+++ server/index.js
@@ -31,7 +31,8 @@ app.listen(port, () => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use(express.static(__dirname + "/public"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
